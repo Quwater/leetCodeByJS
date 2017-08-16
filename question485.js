30b27e5a3764b73edc6f58746bb2e174c37d9b02
Qcloud <qxwlxk@163.com>
2017-08-16 17:03:07 +0800

    [document]question485.js: add comments

--- question485.js
+++ question485.js
@@ -7,6 +7,7 @@ var findMaxConsecutiveOnes = function(nums) {
     var max = 0;
     var cnt = 0;
     for (var i = 0; i < nums.length; i++) {
+        // 每确定一个1，便与 max 比较一次，如果遇到0，重置 cnt
         if (nums[i] === 1) {
             cnt += 1;
             max = Math.max(max, cnt);
