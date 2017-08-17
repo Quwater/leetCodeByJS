063faa7ea4534314d27d95a2142d91dabf73baac
Qcloud <qxwlxk@163.com>
2017-08-17 19:37:02 +0800

    [document]question122.js: add question-122 Best Time to Buy and Sell Stock II

---
+++ question122.js
@@ -0,0 +1,14 @@
+/**
+ * @name 122. Best Time to Buy and Sell Stock II
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var sum = 0;
+
+    // 相邻价位赚钱的全部买进，不赚钱的全部避开
+    for (var i = 0; i < prices.length-1; i++) {
+        sum += Math.max(prices[i+1]-prices[i], 0);
+    }
+    return sum;
+};
