8f40f220eed4b001925d2272a2037da6aa5a0c6e
Qcloud <qxwlxk@163.com>
2017-08-23 20:37:55 +0800

    [document]question189.js: add question Rotate Array

---
+++ question189.js
@@ -0,0 +1,22 @@
+/**
+ * @name Rotate Array
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    var arr = [];
+    var len = nums.length;
+    // 如果nums 里只有一个数值，那么不做任何改变
+    if (len > 1) {
+        //获取最后的 k 个值
+        var nums1 = nums.splice(len-k, k);
+        for (var i = k-1; i >= 0; i--) {
+            // 去除可能的 undefined，将其余的值倒着赋值
+            if (nums1[i] !== undefined) {
+                nums.splice(0, 0, nums1[i]);
+                // nums.unshift(nums1[i]);
+            }
+        }
+    }
+};
