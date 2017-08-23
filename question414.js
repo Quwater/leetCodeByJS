4b3d8742c2aed73ba5a30ebdf431b4825881da89
Qcloud <qxwlxk@163.com>
2017-08-23 19:54:26 +0800

    [document]question414.js: add question Third Maximum Number

---
+++ question414.js
@@ -0,0 +1,15 @@
+/**
+ * @name Third Maximum Number
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    // 利用 set 去除冗余项
+    nums = Array.from(new Set(nums));
+    var len = nums.length;
+    if(len<=2){
+    	return Math.max(...nums);
+    }
+    nums = nums.sort((a,b)=>{return b-a;});
+    return nums[2];
+};
