c3daa37e67d5b5689f145341c49a529fe3ab8b1a
Qcloud <qxwlxk@163.com>
2017-08-26 09:44:11 +0800

    [document]question617.js: add question Merge Two Binary Trees

---
+++ question617.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @name Merge Two Binary Trees
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+var mergeTrees = function(t1, t2) {
+    var root = null;
+    if (t1 && t2) {
+        root =  new TreeNode(null);
+        root.val = (t1.val) + (t2.val);
+        // 递归
+        root.left = mergeTrees(t1.left, t2.left);
+        root.right = mergeTrees(t1.right, t2.right);
+    } else if (t1) {
+        root = t1;
+    } else if (t2){
+        root = t2;
+    }
+    return root;
+};
