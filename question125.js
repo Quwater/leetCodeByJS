8972fb380ce02b82de00006e0c748edcda15051f
Qcloud <qxwlxk@163.com>
2017-08-26 09:26:42 +0800

    [document]question125.js: add question Valid Palindrome

---
+++ question125.js
@@ -0,0 +1,27 @@
+/**
+ * @name Valid Palindrome
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    if (s.length === 0) {
+        return true;
+    }
+    var str = s.toLowerCase();
+    var arr = [];
+    for (var i = 0; i < str.length; i++) {
+        var code = str[i].charCodeAt();
+        // 收入的字符只能为字母或数字
+        if (code >= 65 && code <= 90 || code >= 97 && code <= 122 ||
+             !isNaN(parseInt(str[i]))) {
+            arr.push(str[i]);
+        }
+    }
+
+    for (var j = 0; j < arr.length / 2; j++) {
+        if (arr[j] !== arr[arr.length - j - 1]) {
+            return false;
+        }
+    }
+    return true;
+};
