cf37e723e416e3f2b6907bb3af8e9503eae7d270
Qcloud <qxwlxk@163.com>
2017-09-16 20:26:17 +0800

    [document]question674.js: add question Longest Continuous Increasing Subsequence

---
+++ question674.js
@@ -0,0 +1,27 @@
+/**
+ * @name Longest Continuous Increasing Subsequence
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function(nums) {
+    var len = nums.length;
+    var temp = 1;
+    var max = 1;
+    // 如果为空数组，那么返回0
+    if (len === 0) {
+        return 0;
+    }
+    // 遍历，每次比较，如果为升序，则temp加1，同时与 max 比较，一旦大于max，将其赋给max
+    // 如果出现降序，初始化temp
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] < nums[i+1]) {
+            temp++;
+            if (temp > max) {
+                max = temp;
+            }
+        } else {
+            temp = 1;
+        }
+    }
+    return max;
+};
