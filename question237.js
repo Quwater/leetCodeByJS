90590b4bdc89cfb02425529469cd3c324a08a042
Qcloud <qxwlxk@163.com>
2017-09-17 20:29:15 +0800

    [document]question237.js: add question Delete Node in a Linked List

---
+++ question237.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @name Delete Node in a Linked List
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    // 我们不能删除掉这个节点，于是将节点后面的节点属性赋值给当前节点即可
+    // 相当于复制后面节点到当前节点，同时跳过了后面的节点。
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
