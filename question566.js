4644dcb1200298a8cbcf9b0f7a851742ffeb29eb
Qcloud <qxwlxk@163.com>
2017-09-24 18:57:16 +0800

    [document]question566.js: use ES6

--- question566.js
+++ question566.js
@@ -6,23 +6,24 @@
  * @return {number[][]}
  */
 var matrixReshape = function(nums, r, c) {
-    var matrixR = nums.length;
-    var matrixC = nums[0].length;
-    var numsJoin = [];
-    var newNums = [];
+    let matrixR = nums.length;
+    let matrixC = nums[0].length;
+    let numsJoin = [];
+    let newNums = [];
 
     // 判断是否能够转换矩阵
     if (matrixR * matrixC  === r * c) {
         // 合并矩阵
-        for (var i = 0; i < nums.length; i++) {
+        for (let i = 0; i < nums.length; i++) {
             numsJoin = numsJoin.concat(nums[i]);
         }
         // 将新的数组加入newNums
-        for (var j = 1; j <= r; j++) {
+        for (let j = 1; j <= r; j++) {
             newNums.push(numsJoin.splice(0, c));
         }
         return newNums;
     } else {
         return nums;
     }
+
 };
