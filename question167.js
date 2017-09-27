d281bf772ad7f39e4a610f4d234f676fb36a89b5
Qcloud <qxwlxk@163.com>
2017-09-27 21:25:06 +0800

    [document]question167.js: use ES6 and optimize

--- question167.js
+++ question167.js
@@ -5,14 +5,30 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    var index = [];
-    for (var i = 0; i < numbers.length; i++) {
-        for (var j = numbers.length - 1; j > 0; j--) {
-            if (numbers[i] + numbers[j] == target) {
-                index.push(i+1);
-                index.push(j+1);
-                return index;
-            }
+    // var index = [];
+    // for (var i = 0; i < numbers.length; i++) {
+    //     for (var j = numbers.length - 1; j > 0; j--) {
+    //         if (numbers[i] + numbers[j] == target) {
+    //             index.push(i+1);
+    //             index.push(j+1);
+    //             return index;
+    //         }
+    //     }
+    // }
+    //避免循环两次，改为，左一次，右一次（小于目标，则左边小，大于目标，则右边大）
+    var l = 0,
+        r = numbers.length-1,
+        index = [];
+    while (l < r) {
+        s = numbers[l] + numbers[r];
+        if (s === target) {
+            index.push(l+1);
+            index.push(r+1);
+            return index;
+        } else if (s < target) {
+            l += 1;
+        } else {
+            r -= 1;
         }
     }
 };
