c6a0055d143c825776ed57b6fa8c0f0f776d88ed
Qcloud <qxwlxk@163.com>
2017-09-27 21:06:05 +0800

    [document]question283.js: use ES6 and optimize

--- question283.js
+++ question283.js
@@ -4,31 +4,28 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    var count = 0;
-
-    // 删除‘0’，并添加到数组尾，同时遍历的 i 值暂停一次，次数达到数组长度时，结束
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] === 0) {
-            nums.splice(i, 1);
-            nums.push(0);
-            i--;
-        }
-        count++;
-        if (count == nums.length) {
-            break;
+    // var count = 0;
+    //
+    // // 删除‘0’，并添加到数组尾，同时遍历的 i 值暂停一次，次数达到数组长度时，结束
+    // for (var i = 0; i < nums.length; i++) {
+    //     if (nums[i] === 0) {
+    //         nums.splice(i, 1);
+    //         nums.push(0);
+    //         i--;
+    //     }
+    //     count++;
+    //     if (count == nums.length) {
+    //         break;
+    //     }
+    // }
+    let j = 0;
+    // move all the nonzero elements advance
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[j++] = nums[i];
         }
     }
+    for (;j < nums.length; j++) {
+        nums[j] = 0;
+    }
 };
-
-// 更好的方法，与0互换
-// var moveZeroes = function(nums) {
-//     int j = 0;
-//     for(int i = 0; i < nums.length; i++) {
-//         if(nums[i] != 0) {
-//             int temp = nums[j];
-//             nums[j] = nums[i];
-//             nums[i] = temp;
-//             j++;
-//         }
-//     }
-// };
