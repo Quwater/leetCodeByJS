297cc3f0ce6eda22e8b3a5509b9f216529ea03ed
Qcloud <qxwlxk@163.com>
2017-09-29 18:44:33 +0800

    [document]question121.js: use ES6

--- question121.js
+++ question121.js
@@ -4,8 +4,9 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    var maxCur = 0, maxSoFar = 0;
-    for (var i = 1; i < prices.length; i++) {
+    let maxCur = 0,
+        maxSoFar = 0;
+    for (let i = 1; i < prices.length; i++) {
         // 当前最大值是一个累加的过程，但一旦累加值小于零，便重新开始计算
         maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
         // 当前的最大值与之前最大值比较，若大，交换
