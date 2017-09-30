92d200b1c29dff30224bff4748a907e814dbcc8d
Qcloud <qxwlxk@163.com>
2017-09-30 19:11:42 +0800

    [document]question53.js: use ES6

--- question53.js
+++ question53.js
@@ -6,14 +6,16 @@
 
 // 此题与question121 Best Time to Buy and Sell Stock类似
 
-var maxSubArray = function(nums) {
-    var sumCur = nums[0];
-    var sumSoFar = nums[0];
-    for (var i = 1; i < nums.length; i++) {
-        // 之前和值为负值，则舍掉
+let maxSubArray = function(nums) {
+    let sumCur = nums[0],
+        sumSoFar = nums[0];
 
+    for (let i = 1; i < nums.length; i++) {
+
+        // 之前和值为负值，则舍掉
         sumCur = Math.max(sumCur + nums[i], nums[i]);
         sumSoFar = Math.max(sumSoFar, sumCur);
     }
+
     return sumSoFar;
 };
