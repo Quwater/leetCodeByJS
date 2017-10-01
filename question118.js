5700b19dac1ecc631437e031f9f22465c8184403
Qcloud <qxwlxk@163.com>
2017-10-01 20:44:55 +0800

    [document]question118.js: use ES6

--- question118.js
+++ question118.js
@@ -4,15 +4,15 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    var result = [];
-    for(var i=0; i<numRows; i++){
+    let result = [];
+    for(let i=0; i<numRows; i++){
         result[i] = [];
     }
-    for(i=0; i<numRows; i++){
+    for(let i=0; i<numRows; i++){
         result[i][0] = result[i][i] = 1;
     }
     // 帕斯卡三角元素的值为上面两个数之和
-    for(i=2; i<numRows; i++){
+    for(let i=2; i<numRows; i++){
         for(var j=1; j<i; j++){
             result[i][j] = result[i-1][j-1] + result[i-1][j];
         }
