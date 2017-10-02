dcc0d1412b822e8d08921f3fc3cb6276d2f1b68c
Qcloud <qxwlxk@163.com>
2017-10-02 18:41:47 +0800

    [document]question26.js: use ES6 and optimize

--- question26.js
+++ question26.js
@@ -4,10 +4,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    for (var i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i-1]) {
-            nums.splice(i, 1);
-            i--;
+    // for (var i = 1; i < nums.length; i++) {
+    //     if (nums[i] === nums[i-1]) {
+    //         nums.splice(i, 1);
+    //         i--;
+    //     }
+    // }
+    let len = nums.length;
+    if (len < 2) {
+        return len;
+    }
+    let id = 1;
+    for (let i = 1; i < len; i++) {
+        if (nums[i] !== nums[i-1]) {
+            nums[id++] = nums[i];
         }
     }
+    return num;
 };
