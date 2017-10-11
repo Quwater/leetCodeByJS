d6e8a45265e6283ad0d765cfe64e99539db9c5b3
Qcloud <qxwlxk@163.com>
2017-10-11 18:13:02 +0800

    [document]question532.js: use ES6 and optimize

--- question532.js
+++ question532.js
@@ -5,14 +5,14 @@
  * @return {number}
  */
 var findPairs = function(nums, k) {
-    var num = 0;
-    var arr = [];
-    nums.sort();
-    for (var i = 0; i < nums.length; i++) {
+    let num = 0,
+        arr = [];
+
+    for (let i = 0; i < nums.length; i++) {
        // k >= 0 同时每当有数字加上 k 后的值存在与数组中，且该值不能重复使用，并且当 k 为0时，
        // 保证该数值不为i 索引下的值
-       if (k >= 0 && nums.indexOf(nums[i]+k) !== -1 && nums.indexOf(nums[i]+k) !=
-       i && arr.indexOf(nums[i]) === -1) {
+       if (k >= 0 && nums.indexOf(nums[i]+k) !== -1 && nums.indexOf(nums[i]+k)
+       !== i && arr.indexOf(nums[i]) === -1) {
            num++;
            arr.push(nums[i]);
        }
