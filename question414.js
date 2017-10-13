124e6cf0ec6567df22062db8e0f6e5c0da8a4fb3
Qcloud <qxwlxk@163.com>
2017-10-13 19:00:23 +0800

    [document]question414.js: use ES6

--- question414.js
+++ question414.js
@@ -6,9 +6,9 @@
 var thirdMax = function(nums) {
     // 利用 set 去除冗余项
     nums = Array.from(new Set(nums));
-    var len = nums.length;
-    if(len<=2){
-    	return Math.max(...nums);
+    let len = nums.length;
+    if (len<=2) {
+        return Math.max(...nums);
     }
     nums = nums.sort((a,b)=>{return b-a;});
     return nums[2];
