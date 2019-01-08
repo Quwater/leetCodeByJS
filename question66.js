40e25ef5b7dc2c88bdd5fb4b463dfabd4cf416ee
Qcloud <qxwlxk@163.com>
2019-01-08 14:06:19 +0800

    [Modify]modify question66

--- question66.js
+++ question66.js
@@ -3,17 +3,14 @@
  * @return {number[]}
  */
 let plusOne = function(digits) {
-    let carry = true;
     for (let i = digits.length - 1; i >= 0; i--) {
         if(digits[i] < 9) {
             digits[i]++;
             return digits;
         }
-        
         digits[i] = 0;
     }
-    if (carry) {
-        digits.unshift(1);
-    }
+    digits.unshift(1);
+
     return digits;
 };
