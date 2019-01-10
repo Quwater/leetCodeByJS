2d906ee6c0c47ff894d34673625210cf238f2c79
Qcloud <qxwlxk@163.com>
2019-01-10 20:08:42 +0800

    [document]Add question849

---
+++ question849.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} seats
+ * @return {number}
+ */
+let maxDistToClosest = function(seats) {
+    if (seats.length === 1) return 0;
+    let maxDistance = 0;
+    let lastSeated = -1;
+    for (let i = 0; i < seats.length; i++) {
+        if (seats[i] === 1 && lastSeated === -1) {
+            lastSeated = i;
+            maxDistance = lastSeated - 0;    //[0, 0, 0, 1, ....]
+        } else if (seats[i] === 1 && lastSeated !== -1) {
+            maxDistance = Math.max(maxDistance, Math.floor((i - lastSeated) / 2));
+            lastSeated = i;
+        }
+    }
+    return Math.max(maxDistance, seats.length - 1 - lastSeated ); //[..., 1, 0, 0, ...]
+};
+maxDistToClosest([1,0,0,0,1,0,1]);
