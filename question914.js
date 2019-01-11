4be25fa95eb75a26f3074b47091d7abce8d897ff
Qcloud <qxwlxk@163.com>
2019-01-11 21:27:08 +0800

    [modify]modify question914

--- question914.js
+++ question914.js
@@ -2,7 +2,7 @@
  * @param {number[]} deck
  * @return {boolean}
  */
-let hasGroupsSizeX = function(deck) {
+let hasGroupsSizeX = function (deck) {
     let obj = {};
     for (let i of deck) {
         obj[i] = obj[i] + 1 || 1;
@@ -17,11 +17,11 @@ let hasGroupsSizeX = function(deck) {
     return true;
 };
 
-function gcd(a,b){
-    if (b === 0){
+function gcd(a, b) {
+    if (b === 0) {
         return a;
     }
-    return gcd(b, a%b);
+    return gcd(b, a % b);
 }
 
-hasGroupsSizeX([1,2,3,4,4,3,2,1]);
+hasGroupsSizeX([1, 2, 3, 4, 4, 3, 2, 1]);
