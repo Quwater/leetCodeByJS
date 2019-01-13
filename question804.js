ed9a99a5036de25108624fcf7f104d8027ca4dc6
Qcloud <qxwlxk@163.com>
2019-01-13 20:34:21 +0800

    [document]Add question804

---
+++ question804.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+let uniqueMorseRepresentations = function (words) {
+    const morseList = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+    const getIdx = char => char.charCodeAt(0) - 'a'.charCodeAt(0);
+    
+    let newList = words.map(word => word.split('').map(char => morseList[getIdx(char)]).join(''))
+    return new Set(newList).size;
+};
+uniqueMorseRepresentations();
