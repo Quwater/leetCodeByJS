f99ae319334d889383c74ae863cb30bf6eabf383
Qcloud <qxwlxk@163.com>
2019-01-18 13:02:48 +0800

    [document]Add question788

---
+++ question788.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+let rotatedDigits = function (N) {
+    let count = 0;
+    for (let i = 1; i <= N; i++) {
+        let tmp = i + '';
+        if (!/[347]/g.test(tmp) && /[2569]/g.test(tmp)) {
+            count++;
+        }
+    }
+    return count;
+};
+rotatedDigits(456);
