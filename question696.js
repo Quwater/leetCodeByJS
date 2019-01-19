40caed57f685935f4750a2354c7b63e4095eccc8
Qcloud <qxwlxk@163.com>
2019-01-19 21:55:06 +0800

    [modify]modify question696

--- question696.js
+++ question696.js
@@ -2,18 +2,33 @@
  * @param {string} s
  * @return {number}
  */
-let countBinarySubstrings = function(s) {
-    let count = 0;
-    for (let i = 0; i < s.length-1; i++) {
-        if (s[i] !== s[i+1]) {
-            count++;
-            let j = 1;
-            while (s[i - j] === s[i] && s[i+1] === s[i + j + 1]) {
-                count++;
-                j++;
-            }
+let countBinarySubstrings = function (s) {
+    // let count = 0;
+    // for (let i = 0; i < s.length-1; i++) {
+    //     if (s[i] !== s[i+1]) {
+    //         count++;
+    //         let j = 1;
+    //         while (s[i - j] === s[i] && s[i+1] === s[i + j + 1]) {
+    //             count++;
+    //             j++;
+    //         }
+    //     }
+    // }
+    // return count;
+    let prevRunLength = 0,
+        curRunLength = 1,
+        res = 0;
+    for (let i = 1; i < s.length; i++) {
+        if (s.charAt(i) === s.charAt(i - 1)) {
+            curRunLength++;
+        } else {
+            prevRunLength = curRunLength;
+            curRunLength = 1;
+        }
+        if (prevRunLength >= curRunLength) {
+            res++;
         }
     }
-    return count;
+    return res;
 };
 countBinarySubstrings();
