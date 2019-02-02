a490305b23807e96f71bbb23f233c02fcc6411bf
Qcloud <qxwlxk@163.com>
2019-02-02 14:20:03 +0800

    [modify]add comments

--- question836.js
+++ question836.js
@@ -4,6 +4,7 @@
  * @return {boolean}
  */
 let isRectangleOverlap = function(rec1, rec2) {
+    // left,right,bottom,top.
     let noOverlap = rec1[2] <= rec2[0] || rec1[0] >= rec2[2] || rec1[1] >= rec2[3] || rec1[3] <= rec2[1];
     return (!noOverlap);
 };
