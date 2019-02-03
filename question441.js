6b6943c932a40c271072b4b64db801455a99a1bd
Qcloud <qxwlxk@163.com>
2019-02-03 16:17:46 +0800

    [document]Add question441

---
+++ question441.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let arrangeCoins = function(n) {
+    // O(n)
+    // let ans = 1;
+    // while (sum(ans) <= n) {
+    //     ans++;
+    //     //console.log(sum(ans));
+    // }
+    // return ans-1;
+
+    // O(logn)
+    let imin = 0, imax = n, i;
+    while (imin < imax) {
+        i = Math.floor((imin + imax)/2) + 1;
+        let num = i * (i+1) /2;
+        if ( num <= n ) {
+            imin = i;
+        } else if (num > n) {
+            imax = i - 1;
+        }
+    }
+    return imin;
+};
+// let sum = (n) => n * (n + 1) / 2;
+arrangeCoins(8);
