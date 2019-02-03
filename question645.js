d80a26315dfdfaed1e5f051cfe89c728ca32a3de
Qcloud <qxwlxk@163.com>
2019-02-03 12:25:01 +0800

    [document]Add question645

--- question645.js
+++ question645.js
@@ -3,20 +3,41 @@
  * @return {number[]}
  */
 let findErrorNums = function(nums) {
-    nums.sort((a, b) => (a - b));
-    let dup = 0,
-        arr = [];
-    for (let i = 0; i < nums.length-1; i++) {
-        if (nums[i] === nums[i+1]) {
-            dup = nums[i];
-            arr = nums.slice(0, i).concat(nums.slice(i+1));
-        } 
+    // nums.sort((a, b) => (a - b));
+    // let dup = 0,
+    //     arr = [];
+    // for (let i = 0; i < nums.length-1; i++) {
+    //     if (nums[i] === nums[i+1]) {
+    //         dup = nums[i];
+    //         arr = nums.slice(0, i).concat(nums.slice(i+1));
+    //     } 
+    // }
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] !== i + 1) {
+    //         return [dup, i+1];
+    //     } 
+    // }
+    // return [dup, nums.length];
+    let occursTwice = 0;
+    
+    for (var i = 0; i < nums.length; i++) {
+        var index = Math.abs(nums[i]) - 1;
+        if (nums[index] < 0) {
+            occursTwice = index + 1;
+        }
+        nums[index] = -nums[index];
     }
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== i + 1) {
-            return [dup, i+1];
-        } 
+    
+    let result = [occursTwice];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0 && i + 1 !== occursTwice) {
+            result.push(i + 1);
+            return result;
+        }
+
     }
-    return [dup, nums.length];
+    
+    return result;
 };
 findErrorNums([2, 2, 3]);
