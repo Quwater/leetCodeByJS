c31b3049c968897f15c089da1d312d5893dbd3b5
Qcloud <qxwlxk@163.com>
2019-02-04 22:36:14 +0800

    [document]Add question204

---
+++ question204.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+    let count = 1;
+    let notPrime = {};
+    if (n <= 1) {
+        return 0;
+    }
+
+    if (n === 2) {
+        return 0;
+    }
+
+    // for (let i = 2; i < n; i++) {
+    //     if (!notPrime[i]) {
+    //         count++;
+    //     }
+    //     for (let j = 2; i *j < n; j++) {
+    //             notPrime[i*j] = true;
+    //     }
+    // }
+    const sqrt = Math.sqrt(n);
+
+    for (let i = 3; i < n; i += 2) {
+        if (notPrime[i]) {
+            continue;
+        }
+
+        count++;
+
+        if (i > sqrt) {
+            continue;
+        }
+
+        for (let j = i * i; j < n; j += i * 2) {
+            notPrime[j] = true;
+        }
+    }
+
+    return count;
+};
+countPrimes(10);
