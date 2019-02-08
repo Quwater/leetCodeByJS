a4695a173d36f1ccbecc232d87720b9e6cda6397
Qcloud <qxwlxk@163.com>
2019-02-08 21:08:26 +0800

    [modify]modify question475

--- question475.js
+++ question475.js
@@ -3,16 +3,17 @@
  * @param {number[]} heaters
  * @return {number}
  */
-let findRadius = function(houses, heaters) {
-    houses.sort((a,b) => a-b);
-    heaters.sort((a,b) => a-b);
+let findRadius = function (houses, heaters) {
+    houses.sort((a, b) => a - b);
+    heaters.sort((a, b) => a - b);
 
     let min = 0;
     let j = 0;
 
-    for(let i = 0 ;i < houses.length ;i++){
-        while( j < heaters.length - 1 && (Math.abs(heaters[j] - houses[i]) >= Math.abs(heaters[j+1] - houses[i])))
-            j ++;
+    for (let i = 0; i < houses.length; i++) {
+        // left < right
+        while (j < heaters.length - 1 && (Math.abs(heaters[j] - houses[i]) >= Math.abs(heaters[j + 1] - houses[i])))
+            j++;
         min = Math.max(min, Math.abs(heaters[j] - houses[i]));
     }
     return min;
