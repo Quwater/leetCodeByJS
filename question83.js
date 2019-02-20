5686e3b788807b45986f4d809fb2b30947d6563e
Qcloud <qxwlxk@163.com>
2019-02-20 20:01:43 +0800

    [modify]modify question83

--- question83.js
+++ question83.js
@@ -10,24 +10,43 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-    var first = head;
-    // 遍历链表
-    while(head && head.next) {
-        var next = head.next;
-        if (head.val === next.val) {
-            // 如果下下个元素不为空，则通过复制方式删掉next
-            if (next.next !== null) {
-                next.val = next.next.val;
-                next.next = next.next.next;
-            } else {
-            // 如果下下个元素为空,直接将末尾元素删掉即可
-                head.next = null;
-                return first;
-            }
+let deleteDuplicates = function (head) {
+    // let first = head;
+    // // 遍历链表
+    // while(head && head.next) {
+    //     let next = head.next;
+    //     if (head.val === next.val) {
+    //         // 如果下下个元素不为空，则跳过next
+    //         if (next.next !== null) {
+    //             next.val = next.next.val;
+    //             next.next = next.next.next;
+    //         } else {
+    //         // 如果下下个元素为空,直接将末尾元素删掉即可
+    //             head.next = null;
+    //             return first;
+    //         }
+    //     } else {
+    //         head = next;
+    //     }
+    // }
+    // return first;
+    if (!head) {
+        return head;
+    }
+
+    let first = head,
+        second = head.next;
+    while (second != null) {
+        // remove repeat element
+        if (first.val === second.val) {
+            first.next = second.next;
+            second = second.next;
         } else {
-            head = next;
+            first = second;
+            second = second.next;
         }
     }
-    return first;
+
+    return head;
 };
+deleteDuplicates();
