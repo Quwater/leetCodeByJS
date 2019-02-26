8bde22422423acd31258f7b744be2138aeed872e
Qcloud <qxwlxk@163.com>
2019-02-26 15:33:00 +0800

    [modify]Add a new way

--- question141.js
+++ question141.js
@@ -11,7 +11,8 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function(head) {
+// O(n)
+let hasCycle = function (head) {
     // 使用了额外的空间
     // while (head && head.next) {
     //     if (!head.hasOwnProperty('rem')) {
@@ -23,19 +24,31 @@ var hasCycle = function(head) {
     // }
     // return false;
 
-    if(head === null) {
+    // let map = [];
+    // while (head != null) {
+    //     if (map.includes(head)) {
+    //         return true;
+    //     } else {
+    //         map.push(head);
+    //     }
+    //     head = head.next;
+    // }
+    // return false;
+
+    if (head === null) {
         return false;
     }
-    var walker = head;
-    var runner = head;
+    let walker = head;
+    let runner = head;
     // 使用两个节点遍历链表，其中一个速度为另外一个的两倍，若出现节点重复的状态
     // 则说明快的那个追上了慢的那个节点，必然存在圆环
-    while(runner.next !==null && runner.next.next != null) {
+    while (runner.next !== null && runner.next.next != null) {
         walker = walker.next;
         runner = runner.next.next;
-        if(walker === runner) {
-           return true;
+        if (walker === runner) {
+            return true;
         }
     }
     return false;
 };
+hasCycle();
