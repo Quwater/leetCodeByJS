e3aae485608bda4391aee914c9d21db29b4dff17
Qcloud <qxwlxk@163.com>
2019-02-27 13:34:32 +0800

    [modify]modify question160

--- question160.js
+++ question160.js
@@ -12,22 +12,33 @@
  * @param {ListNode} headB
  * @return {ListNode}
  */
-var getIntersectionNode = function(headA, headB) {
-    //boundary check
-    if (headA == null || headB == null) {
-       return null;
-    }
 
-    var a = headA;
-    var b = headB;
+// O(n)
+let getIntersectionNode = function (headA, headB) {
+    let a = headA,
+        b = headB;
 
     //if a & b have different len, then we will stop the loop after second iteration
     // 类似两个梯形组成一个矩形
-    while( a !== b){
-    	//for the end of first iteration, we just reset the pointer to the head of another linkedlist
-        a = a === null? headB : a.next;
-        b = b === null? headA : b.next;
+    while (a !== b) {
+        //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        a = a ? a.next : headB;
+        b = b ? b.next : headA;
     }
 
     return a;
+
+    // let set = new Set();
+    // while (headA) {
+    //     set.add(headA);
+    //     headA = headA.next;
+    // }
+    // while (headB) {
+    //     if (set.has(headB)) {
+    //         return headB;
+    //     }
+    //     headB = headB.next;
+    // }
+    // return null;
 };
+getIntersectionNode();
