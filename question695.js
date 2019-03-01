2ace58d5cd0b1425728ed4be4dd88e40df5d3aac
Qcloud <qxwlxk@163.com>
2019-03-01 19:44:22 +0800

    [document]Add question695

---
+++ question695.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+// O(n2)
+let maxAreaOfIsland = function (grid) {
+    let ans = 0,
+        seen = [];
+    for (let i = 0; i < grid.length; i++) {
+        seen[i] = [];
+        for (let j = 0; j < grid[0].length; j++) {
+            seen[i][j] = false;
+        }
+    }
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            ans = Math.max(ans, area(r, c));
+        }
+    }
+    return ans;
+
+
+    function area(r, c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length ||
+            seen[r][c] || grid[r][c] === 0) {
+            return 0;
+        }
+        seen[r][c] = true;
+        return (1 + area(r + 1, c) + area(r - 1, c) + area(r, c - 1) + area(r, c + 1));
+    }
+};
+
+maxAreaOfIsland([[0,1]]);
