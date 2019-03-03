988826405d4ed17acd67966c9d593776702665ae
Qcloud <qxwlxk@163.com>
2019-03-03 19:27:10 +0800

    [document]Add question811

---
+++ question811.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} cpdomains
+ * @return {string[]}
+ */
+// O(n)
+let subdomainVisits = function (cpdomains) {
+    let table = {},
+        res = [];
+    for (let i of cpdomains) {
+        let arr = i.split(' '),
+            domain = arr[1];
+        while (domain) {
+            table[domain] = table[domain] ? table[domain] + parseInt(arr[0]) : parseInt(arr[0]);
+            if (domain.indexOf('.') !== -1) {
+                domain = domain.slice(domain.indexOf('.') + 1);
+            } else {
+                domain = '';
+            }
+        }
+    }
+    for (let i in table) {
+        res.push(table[i] + ' ' + i);
+    }
+    return res;
+};
+subdomainVisits(['9001 discuss.leetcode.com']);
