5fbc33a285da84950ce45ecf4ce7e73d1696d4d9
Qcloud <qxwlxk@163.com>
2019-03-05 18:59:35 +0800

    [modify]Add a new method

--- question136.js
+++ question136.js
@@ -4,11 +4,20 @@
  */
 // O(n)
 let singleNumber = function(nums) {
-    let res = 0;
+    // let res = 0;
+    // for (let i of nums) {
+    //     // use XOR two times
+    //     res ^= i;
+    // }
+    // return res;
+    let map = {};
     for (let i of nums) {
-        // use XOR two times
-        res ^= i;
+        if (map[i]) {
+            delete map[i];
+        } else {
+            map[i] = 1;
+        }
     }
-    return res;
+    return Object.keys(map)[0];
 };
 singleNumber([1, 2, 2]);
