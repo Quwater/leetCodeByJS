995fe0878d3c331f74bafc69b9720a291ff9c2f4
Qcloud <qxwlxk@163.com>
2019-03-08 18:13:45 +0800

    [modify]Add a new method

--- question387.js
+++ question387.js
@@ -2,12 +2,23 @@
  * @param {string} s
  * @return {number}
  */
+// O(n)
 let firstUniqChar = function(s) {
+    // for (let i of s) {
+    //     if (s.indexOf(i) === s.lastIndexOf(i)) {
+    //         return s.charAt(i);
+    //     }
+    // }
+    // return -1;
+    let map = {};
     for (let i of s) {
-        if (s.indexOf(i) === s.lastIndexOf(i)) {
-            return s.charAt(i);
+        map[i] = map[i] + 1 || 1;
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (map[s[i]] === 1) {
+            return i;
         }
     }
     return -1;
 };
-firstUniqChar();
+firstUniqChar('leetcode');
