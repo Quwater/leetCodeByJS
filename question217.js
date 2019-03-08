9bda4c99d9d56de10387e2da2b5b044febfecc51
Qcloud <qxwlxk@163.com>
2019-03-08 11:42:00 +0800

    [modify]use a clever method

--- question217.js
+++ question217.js
@@ -1,15 +1,9 @@
 /**
- * @name 217. Contains Duplicate
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    nums.sort();
-    // 排序后降低算法复杂度
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] === nums[i-1]) {
-            return true;
-        }
-    }
-    return false;
+// O(n)
+let containsDuplicate = function(nums) {
+    return new Set(nums).size !== nums.length;
 };
+containsDuplicate([1, 2, 3, 1]);
