5b54550081391fc31a77d54aca40f044cc73fe69
Qcloud <qxwlxk@163.com>
2019-03-29 19:37:01 +0800

    [docs]Add question501.js

---
+++ question501.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+let findMode = function (root) {
+    // let map = {},
+    //     max = 0,
+    //     res = [];
+    // let helper = function(node) {
+    //     if (!node) {
+    //         return;
+    //     }
+    //     map[node.val] = map[node.val] + 1 || 1;
+    //     max = Math.max(max, map[node.val]);
+    //     helper(node.left);
+    //     helper(node.right);
+    // }
+    // helper(root);
+    // for (let i in map) {
+    //     if (map[i] === max) {
+    //         res.push(i);
+    //     }
+    // }
+    // return res;
+
+    if (!root) {
+        return [];
+    }
+    let res = [],
+        max = 0,
+        curVal = root.val,
+        count = 0;
+    let dfs = function (node) {
+        if (!node) {
+            return;
+        }
+        dfs(node.left);
+        if (node.val === curVal) {
+            count++;
+        } else {
+            count = 1;
+        }
+        if (count > max) {
+            res = [];
+            res.push(node.val);
+        } else if (count === max) {
+            res.push(node.val);
+        }
+        max = Math.max(count, max);
+        curVal = node.val;
+        dfs(node.right);
+    };
+    dfs(root);
+    return res;
+};
+findMode();
