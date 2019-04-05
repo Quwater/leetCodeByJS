47b5369fdd52a081b7de5353fc81e4030dc7847f
Qcloud <qxwlxk@163.com>
2019-04-05 20:36:52 +0800

    [docs]Use a new method

--- question565.js
+++ question565.js
@@ -21,16 +21,34 @@ let arrayNesting = function (nums) {
     //     set.clear();
     // }
     // return max;
-    let maxSize = 0;
+
+    // time O(n) space O(1)
+    // let maxSize = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     let size = 0;
+    //     for (let j = i; nums[j] >= 0; size++) {
+    //         const nextIndex = nums[j];
+    //         nums[j] = -1;
+    //         j = nextIndex;
+    //     }
+    //     maxSize = Math.max(maxSize, size);
+    // }
+    // return maxSize;
+
+    // time O(n) space O(n)
+    let visited = new Array(nums.length),
+        res = 0;
     for (let i = 0; i < nums.length; i++) {
-        let size = 0;
-        for (let j = i; nums[j] >= 0; size++) {
-            const nextIndex = nums[j];
-            nums[j] = -1;
-            j = nextIndex;
+        if (!visited[i]) {
+            let start = nums[i], count = 0;
+            do {
+                start = nums[start];
+                count++;
+                visited[start] = true;
+            } while (start !== nums[i]);
+            res = Math.max(res, count);
         }
-        maxSize = Math.max(maxSize, size);
     }
-    return maxSize;
+    return res;
 };
 arrayNesting([5, 4, 0, 3, 1, 6, 2]);
