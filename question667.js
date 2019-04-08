d6e7a17e10126f35f97508ad5ad62960845d2c04
Qcloud <qxwlxk@163.com>
2019-04-08 21:11:55 +0800

    [docs]Add a new method

--- question667.js
+++ question667.js
@@ -4,16 +4,25 @@
  * @param {number} k
  * @return {number[]}
  */
-var constructArray = function(n, k) {
-    var arr = [];
-    arr.length = n;
+let constructArray = function (n, k) {
+    // let res = new Array(n);
+    // let c = 0;
+    // for (let v = 1; v < n - k; v++) {
+    //     res[c++] = v;
+    // }
+    // for (let i = 0; i <= k; i++) {
+    //     res[c++] = (i % 2 === 0) ? (n-k + i/2) : (n - i/2);
+    // }
+    // return res;
+    let res = new Array(n);
     for (var i = 0; i < n; i++) {
-        arr[i] = i+1;
+        res[i] = i + 1;
     }
     // 每一次转移最后一项，增加两种差值，k-=2
-    for (;k > 1; k-=2) {
-        var a = arr.splice(n-1, 1);
-        arr.splice(n-k, 0, a[0]);
+    for (; k > 1; k -= 2) {
+        let a = res.pop();
+        res.splice(n - k, 0, a);
     }
-    return arr;
+    return res;
 };
+constructArray(3, 2);
