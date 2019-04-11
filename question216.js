0bdcb541971c9fc50d5557a2ccb66b9481fad174
Qcloud <qxwlxk@163.com>
2019-04-11 20:12:25 +0800

    [docs]Add a new method

--- question216.js
+++ question216.js
@@ -4,40 +4,22 @@
  * @param {number} n
  * @return {number[][]}
  */
-var combinationSum3 = function(k, n) {
-    var solution = [];
-	var result = [];
-	var used = [];
+let combinationSum3 = function (k, n) {
+    let res = [];
 
-	var backTracking = function(m, n) {
-		if(m === k){
-			var sum = 0;
-			for (var i=0; i<solution.length; i++) {
-				sum += solution[i];
-			}
-			if (sum === n) {
-				result.push(solution.slice(0));
-			}
-		} else {
-			for (var i=1; i<=9; ++i) {
-                // 如果重复了，跳过
-				if(used[i]){
-                    continue;
-                }
-                // 元素只能从小到大
-				if (m>0 && solution[m-1]>i) {
-                    continue;
-                }
-				used[i] = true;
-				solution[m] = i;
-                // 递归
-				arguments.callee(m+1, n);
-				used[i] = false;
-			}
-		}
-	}
-
-	backTracking(0, n);
-
-	return result;
+    function helper(temp, need, start) {
+        if (temp.length === k && need === 0) {
+            res.push(temp.slice());
+        } else {
+            for (let i = start; i <= 9; i++) {
+                if (i > need) break;
+                temp.push(i);
+                helper(temp, need - i, i + 1);
+                temp.pop();
+            }
+        }
+    }
+    helper([], n, 1);
+    return res;
 };
+combinationSum3(3, 7);
