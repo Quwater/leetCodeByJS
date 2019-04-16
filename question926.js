a87410ff78c27a685eef2dfa5df235f46c23fc69
Qcloud <qxwlxk@163.com>
2019-04-16 20:32:34 +0800

    [docs]Add comments

--- question926.js
+++ question926.js
@@ -8,6 +8,8 @@ let minFlipsMonoIncr = function(S) {
     for (let i = 0; i < N; ++i)
         P[i+1] = P[i] + (S.charAt(i) == '1' ? 1 : 0);
     let ans = Number.MAX_SAFE_INTEGER;
+    // P[j], 1 to 0
+    // N-j-(P[N]-P[j], 0 to 1
     for (let j = 0; j <= N; ++j) {
         ans = Math.min(ans, P[j] + N-j-(P[N]-P[j]));
     }
