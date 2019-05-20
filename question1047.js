1c1e28d9985a0439ed7f8c020f20f1225dcdec5f
Qcloud <qxwlxk@163.com>
2019-05-20 09:49:51 +0800

    [docs]Add question1047

---
+++ question1047.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} S
+ * @return {string}
+ */
+let removeDuplicates = function(S) {
+    let res = [];
+    for(let i=0; i<S.length; i++){
+        if(S[i] !== res[res.length-1]) {
+            res.push(S[i]);
+        } else {
+            res.pop();
+        }
+    }
+    return res.join('');
+};
+removeDuplicates('abbaca');
