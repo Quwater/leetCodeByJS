9cd482ee53d221a9b63aa8b1382bd2ff31f4e1e3
Qcloud <qxwlxk@163.com>
2019-06-16 12:07:37 +0800

    [docs]Add question1086

---
+++ question1086.js
@@ -0,0 +1,30 @@
+/**
+ * @name High Five
+ * @param {number[][]} items
+ * @return {number[][]}
+ */
+let highFive = function(items) {
+    let arrId = [],
+        res = [];
+    for (let i of items) {
+        if (! arrId.includes(i[0])) {
+            arrId.push(i[0]);
+        }
+    }
+    let score = [];
+    for (let i = 0; i < arrId.length; i++) {
+        for (let j = 0; j < items.length; j++) {
+            if (items[j][0] === arrId[i]) {
+                score.push(items[j][1]);
+            }
+        }
+        score.sort((a, b) => b - a);
+        let sum = 0
+        for (let k = 0; k < 5; k++) {
+            sum += score[k]
+        }
+        res.push([arrId[i], Math.floor(sum / 5)]);
+        score = [];
+    }
+    return res;
+};
