c812f280893f516c03b63f5f772f0acba745453b
Qcloud <qxwlxk@163.com>
2020-01-19 13:08:04 +0800

    [docs]Add question1313

---
+++ question1313.js
@@ -0,0 +1,14 @@
+/**
+ * @name Decompress Run-Length Encoded List
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const decompressRLElist = function(nums) {
+    let res = [];
+    for (let i = 0; 2 * i + 1 < nums.length; i++) {
+        for (let j = 0; j < nums[2 * i]; j++) {
+            res.push(nums[2 * i + 1]);
+        }
+    }
+    return res;
+};
