567092ff4c9ad44f1ae872c2531822b8dd679570
Qcloud <qxwlxk@163.com>
2020-06-07 08:04:21 +0800

    [docs]Add question1460

---
+++ question1460.js
@@ -0,0 +1,19 @@
+/**
+ * @name Make Two Arrays Equal by Reversing Sub-arrays
+ * @param {number[]} target
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+const canBeEqual = function (target, arr) {
+    if (target.length !== arr.length) {
+        return false;
+    }
+    const newArr1 = target.sort((a, b) => a - b);
+    const newArr2 = arr.sort((a, b) => a - b);
+    for (let i = 0; i < newArr1.length; i++) {
+        if (newArr1[i] !== newArr2[i]) {
+            return false;
+        }
+    }
+    return true;
+};
