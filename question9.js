8958f9d8575c955f3bfad5e618cf081d355a2191
Qcloud <qxwlxk@163.com>
2020-06-10 12:23:56 +0800

    [docs]Add a new way

--- question9.js
+++ question9.js
@@ -1,13 +1,26 @@
 /**
+ * @name Palindrome Number
  * @param {number} x
  * @return {boolean}
  */
-let isPalindrome = function(x) {
-    let str = x.toString();
-    if (str.split('').reverse().join('') === str) {
-        return true;
-    } else {
+const isPalindrome = function (x) {
+    // O(n)
+    // let str = x.toString();
+    // if (str.split('').reverse().join('') === str) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    // O(logn)
+    if (x < 0 || (x % 10 === 0 && x !== 0)) {
         return false;
     }
+    let reverseNum = 0;
+    while (x > reverseNum) {
+        reverseNum = reverseNum * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+    return x === reverseNum || x === Math.floor(reverseNum / 10);
 };
-isPalindrome();
+isPalindrome(121);
