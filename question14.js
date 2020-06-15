c47962549f6438dcc7b9d0a21f06f54587d37102
Qcloud <qxwlxk@163.com>
2020-06-15 11:53:58 +0800

    [docs]replace a new method

--- question14.js
+++ question14.js
@@ -3,15 +3,28 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
-    var prefix = '';
-    for (var i = 0; strs.length > 0; prefix+=strs[0][i], i++) {
-        for (var j = 0; j < strs.length; j++) {
-            // 如果超出其他字符串元素长度，或者当前索引的值与前一个元素索引的值不同，则返回 prefix
-            if (i >= strs[j].length || (j > 0 && strs[j][i] !== strs[j-1][i])) {
-                return prefix;
+const longestCommonPrefix = function (strs) {
+    let res = '';
+    if (strs.length === 0) {
+        return res;
+    }
+    if (strs.length === 1) {
+        return strs[0];
+    }
+    for (let i = 0; strs.length > 0; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (
+                i >= strs[0].length ||
+                i >= strs[j].length ||
+                strs[j][i] !== strs[0][i]
+            ) {
+                return res;
+            }
+            if (j === strs.length - 1) {
+                res += strs[0][i];
             }
         }
     }
-    return prefix;
 };
+
+longestCommonPrefix(['dog', 'race', 'car']);
