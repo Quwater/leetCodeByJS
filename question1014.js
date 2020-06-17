e0cfdfb5c01f017a9c7c93db283bfee44d04c183
Qcloud <qxwlxk@163.com>
2020-06-17 20:43:41 +0800

    [docs]Add comment

--- question1014.js
+++ question1014.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 // O(N)
-let maxScoreSightseeingPair = function(A) {
+let maxScoreSightseeingPair = function (A) {
     let res = -Infinity,
         temp = A[0];
     for (let i = 1; i < A.length; i++) {
+        // 等价于求[0,j−1] 中 A[i]+i的最大值 mx
         res = Math.max(res, temp + A[i] - i);
         temp = Math.max(temp, A[i] + i);
     }
     return res;
 };
-maxScoreSightseeingPair([8,1,5,2,6]);
+maxScoreSightseeingPair([8, 1, 5, 2, 6]);
