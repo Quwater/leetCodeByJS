7ff757033d1e0c4ad843967544b011c428baeefd
Qcloud <qxwlxk@163.com>
2020-06-19 12:14:37 +0800

    [docs]Add a new way

--- question125.js
+++ question125.js
@@ -3,25 +3,33 @@
  * @param {string} s
  * @return {boolean}
  */
-var isPalindrome = function(s) {
-    if (s.length === 0) {
-        return true;
-    }
-    var str = s.toLowerCase();
-    var arr = [];
-    for (var i = 0; i < str.length; i++) {
-        var code = str[i].charCodeAt();
-        // 收入的字符只能为字母或数字
-        if (code >= 65 && code <= 90 || code >= 97 && code <= 122 ||
-             !isNaN(parseInt(str[i]))) {
-            arr.push(str[i]);
-        }
-    }
-
-    for (var j = 0; j < arr.length / 2; j++) {
-        if (arr[j] !== arr[arr.length - j - 1]) {
+const isPalindrome = function (s) {
+    let str = s.replace(/\W/g, '').toLowerCase();
+    for (let i = 0, j = str.length - 1; i <= j; i++, j--) {
+        if (str[i] !== str[j]) {
             return false;
         }
     }
     return true;
+    // if (s.length === 0) {
+    //     return true;
+    // }
+    // var str = s.toLowerCase();
+    // var arr = [];
+    // for (var i = 0; i < str.length; i++) {
+    //     var code = str[i].charCodeAt();
+    //     // 收入的字符只能为字母或数字
+    //     if (code >= 65 && code <= 90 || code >= 97 && code <= 122 ||
+    //          !isNaN(parseInt(str[i]))) {
+    //         arr.push(str[i]);
+    //     }
+    // }
+
+    // for (var j = 0; j < arr.length / 2; j++) {
+    //     if (arr[j] !== arr[arr.length - j - 1]) {
+    //         return false;
+    //     }
+    // }
+    // return true;
 };
+isPalindrome('A man, a plan, a canal: Panama');
