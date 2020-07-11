76c2a552067cbf227481240180e83c1ec08b3266
Qcloud <qxwlxk@163.com>
2020-07-11 11:00:38 +0800

    [docs]modify

--- question121.js
+++ question121.js
@@ -3,14 +3,26 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
+const maxProfit = function (prices) {
+    // let min = Number.MAX_SAFE_INTEGER,
+    //     max = 0;
+    // for (let i = 0; i < prices.length; i++) {
+    //     if (prices[i] < min) {
+    //         min = prices[i];
+    //     } else {
+    //         max = Math.max(max, prices[i] - min);
+    //     }
+    // }
+    // return max;
     let maxCur = 0,
         maxSoFar = 0;
     for (let i = 1; i < prices.length; i++) {
         // 当前最大值是一个累加的过程，但一旦累加值小于零，便重新开始计算
-        maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+        maxCur = Math.max(0, (maxCur += prices[i] - prices[i - 1]));
         // 当前的最大值与之前最大值比较，若大，交换
         maxSoFar = Math.max(maxSoFar, maxCur);
     }
     return maxSoFar;
 };
+
+maxProfit([7, 1, 5, 3, 6, 2]);
