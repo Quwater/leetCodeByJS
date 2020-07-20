e28ce8832666ffc3f146b19c2b714079174d3cfe
Qcloud <qxwlxk@163.com>
2020-07-20 08:14:36 +0800

    [docs]Add a new method

--- question167.js
+++ question167.js
@@ -4,26 +4,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(numbers, target) {
-    // var index = [];
-    // for (var i = 0; i < numbers.length; i++) {
-    //     for (var j = numbers.length - 1; j > 0; j--) {
-    //         if (numbers[i] + numbers[j] == target) {
-    //             index.push(i+1);
-    //             index.push(j+1);
-    //             return index;
-    //         }
+const twoSum = function (numbers, target) {
+    // let map = new Map();
+    // for (let i = 0; i < numbers.length; i++) {
+    //     if (map.get(target - numbers[i])) {
+    //         return [map.get(target - numbers[i]), i+1];
     //     }
+    //     map.set(numbers[i], i+1);
     // }
-    //避免循环两次，改为，左一次，右一次（小于目标，则左边小，大于目标，则右边大）
+
+    // 双指针
+    // 避免循环两次，改为，左一次，右一次（小于目标，则左边小，大于目标，则右边大）
     let l = 0,
-        r = numbers.length-1,
+        r = numbers.length - 1,
         index = [];
     while (l < r) {
-        s = numbers[l] + numbers[r];
+        let s = numbers[l] + numbers[r];
         if (s === target) {
-            index.push(l+1);
-            index.push(r+1);
+            index.push(l + 1);
+            index.push(r + 1);
             return index;
         } else if (s < target) {
             l += 1;
@@ -32,3 +31,5 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+twoSum([2, 7, 11, 15], 9);
