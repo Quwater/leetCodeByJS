39e0f69ed7d97627ec3f1abeb98ba863d9ff7c5a
Qcloud <qxwlxk@163.com>
2020-08-05 11:05:43 +0800

    [docs]Add a new way -- dp

--- question53.js
+++ question53.js
@@ -6,16 +6,31 @@
 
 // 此题与question121 Best Time to Buy and Sell Stock类似
 
-let maxSubArray = function(nums) {
-    let sumCur = nums[0],
-        sumSoFar = nums[0];
+let maxSubArray = function (nums) {
+    // let sumCur = nums[0],
+    //     sumSoFar = nums[0];
 
-    for (let i = 1; i < nums.length; i++) {
+    // for (let i = 1; i < nums.length; i++) {
 
-        // 之前和值为负值，则舍掉
-        sumCur = Math.max(sumCur + nums[i], nums[i]);
-        sumSoFar = Math.max(sumSoFar, sumCur);
+    //     // 之前和值为负值，则舍掉
+    //     sumCur = Math.max(sumCur + nums[i], nums[i]);
+    //     sumSoFar = Math.max(sumSoFar, sumCur);
+    // }
+
+    // return sumSoFar;
+    let res = -100;
+    let arr = new Array(nums.length + 1);
+    arr[0] = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (arr[i] < 0) {
+            arr[i + 1] = nums[i];
+        } else {
+            arr[i + 1] = nums[i] + arr[i];
+        }
+        if (res < arr[i + 1]) {
+            res = arr[i + 1];
+        }
     }
-
-    return sumSoFar;
+    return res;
 };
+maxSubArray();
