9ba46f7327432e590f7b57f72cd05a61e3adfaf3
Qcloud <qxwlxk@163.com>
2020-08-06 10:10:46 +0800

    [docs]Add question233

---
+++ question233.js
@@ -0,0 +1,16 @@
+/**
+ * @name Number of Digit One
+ * @param {number} n
+ * @return {number}
+ */
+const countDigitOne = function (n) {
+    let countr = 0;
+    for (let i = 1; i <= n; i *= 10) {
+        let divider = i * 10;
+        countr +=
+            Math.floor(n / divider) * i +
+            Math.min(Math.max((n % divider) - i + 1, 0), i);
+    }
+    return countr;
+};
+countDigitOne(13);
