789d7762a953561fe33db3742482b0177c357ed0
Qcloud <qxwlxk@163.com>
2020-08-24 08:25:11 +0800

    [docs]Add a new approach

--- question459.js
+++ question459.js
@@ -3,20 +3,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var repeatedSubstringPattern = function(s) {
-    var subs = '';
-    for (var i = 1; i <= s.length/2; i++){
-        if (s.length % i === 0){
-            subs = s.substr(0,i);
-            var news = '';
-            // i的整倍数，全部加起来如果与 s 相同，便返回正确
-            for (var j = 0; j < s.length / i; j++) {
-                news += subs;
-            }
-            if (news === s) {
-                return true;
-            }
-        }
-    }
-    return false;
+const repeatedSubstringPattern = function (s) {
+    // let res = '';
+    // for (let i = 1; i <= s.length / 2; i++) {
+    //     if (s.length % i === 0) {
+    //         let temp = s.substring(0, i);
+    //         let str = '';
+    //         for (let j = 1; j <= s.length / i; j++) {
+    //             str += temp;
+    //         }
+    //         if (str === s) {
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return false;
+
+    // 如果从第二个元素开始算起，能够找到一个 S。且它是在两个 s 中间，则证明肯定是由重复的子串组成
+    return (s + s).indexOf(s, 1) != s.length;
 };
+
+repeatedSubstringPattern('abba');
