ee7af89a0ed0b9924897985b4395d55034ae1202
Qcloud <qxwlxk@163.com>
2020-08-28 07:03:59 +0800

    [docs]modify question657

--- question657.js
+++ question657.js
@@ -4,30 +4,24 @@
  * @return {boolean}
  */
 
- var judgeCircle = function(moves) {
-    var arr = moves.split('');
-    var x = 0;
-    var y = 0;
-    for (var i = 0; i < arr.length; i++) {
-        switch(arr[i]) {
+const judgeCircle = function (moves) {
+    let x = 0,
+        y = 0;
+    for (let i of moves) {
+        switch (i) {
             case 'U':
-                y += 1;
+                y++;
                 break;
             case 'R':
-                x += 1;
+                x++;
                 break;
             case 'D':
-                y -= 1;
+                y--;
                 break;
             case 'L':
-                x -= 1;
+                x--;
                 break;
         }
     }
-
-    if (x === 0 && y === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return x === 0 && y === 0;
 };
