658187421e4b393fede9dfc4a1a87bc403c9412a
Qcloud <qxwlxk@163.com>
2020-08-30 09:42:20 +0800

    [docs]Add a new solution

--- question557.js
+++ question557.js
@@ -3,12 +3,13 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-    var list = [];
-    var arr = s.split(' ');
-    // arr数组内部的字符串元素是没有变的，所以需要用list来存储结果
-    for (var i = 0; i < arr.length; i++) {
-        list.push(arr[i].split('').reverse().join(''));
-    }
-    return list.join(' ');
+const reverseWords = function (s) {
+    // var list = [];
+    // var arr = s.split(' ');
+    // // arr数组内部的字符串元素是没有变的，所以需要用list来存储结果
+    // for (var i = 0; i < arr.length; i++) {
+    //     list.push(arr[i].split('').reverse().join(''));
+    // }
+    // return list.join(' ');
+    return s.split('').reverse().join('').split(' ').reverse().join(' ');
 };
