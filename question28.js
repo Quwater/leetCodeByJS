b909ec344f6fb498c9c98ec761b526ff3c729770
Qcloud <qxwlxk@163.com>
2020-08-31 21:28:43 +0800

    [docs]Add a new solution

--- question28.js
+++ question28.js
@@ -4,20 +4,18 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    for (var i = 0; ; i++) {
-        for (var j = 0; ; j++) {
-            // 证明一直相等到 needle结束，所以返回 i
-            if (j === needle.length) {
+const strStr = function (haystack, needle) {
+    if (needle === '') {
+        return 0;
+    }
+    let len = haystack.length - needle.length;
+    for (let i = 0; i <= len; i++) {
+        if (haystack[i] === needle[0]) {
+            if (haystack.slice(i, i + needle.length) === needle) {
                 return i;
-            }
-            // 证明一直计算到超出了 haystack 的范围，所以返回-1
-            if (i + j === haystack.length) {
-                return -1;
-            }
-            if (needle.charAt(j) != haystack.charAt(i+j)) {
-                break;
             }
         }
     }
+    return -1;
 };
+strStr('hello', 'll');
