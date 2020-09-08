e03522ec3bf00c0b817a08b6c6b3602ee1165784
Qcloud <qxwlxk@163.com>
2020-09-08 11:02:02 +0800

    [docs]Add a new solution

--- question326.js
+++ question326.js
@@ -2,24 +2,23 @@
  * @param {number} n
  * @return {boolean}
  */
-let isPowerOfThree = function(n) {
-    // let num = 0;
-    // while(Math.pow(3, num) !== n) {
-    //     num++;
-    //     if (Math.pow(3, num) > n) {
+let isPowerOfThree = function (n) {
+    // if (n < 1) {
+    //     return false;
+    // }
+    // while (n > 1) {
+    //     n = n / 3;
+    //     if (n !== parseInt(n)) {
     //         return false;
     //     }
     // }
     // return true;
-    if (n < 1) {
+    if (Math.floor(n) !== n || n < 1) {
         return false;
     }
-    while (n > 1) {
-        n = n / 3;
-        if (n !== parseInt(n)) {
-            return false;
-        }
+    if (n === 1) {
+        return true;
     }
-    return true;
+    return isPowerOfThree(n / 3);
 };
 isPowerOfThree(9);
