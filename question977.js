2041c709e41ed988968bfaf19e1733539fa9e3b0
Qcloud <qxwlxk@163.com>
2020-10-16 16:48:32 +0800

    [docs]Add a new solution

--- question977.js
+++ question977.js
@@ -2,7 +2,20 @@
  * @param {number[]} A
  * @return {number[]}
  */
-let sortedSquares = function(A) {
-    return A.map(value => Math.pow(value, 2)).sort((a, b) => a - b);
+const sortedSquares = function (A) {
+    // return A.map(value => Math.pow(value, 2)).sort((a, b) => a - b);
+    let i = 0,
+        j = A.length - 1;
+    const res = [];
+    while (i <= j) {
+        if (A[i] * A[i] >= A[j] * A[j]) {
+            res.unshift(A[i] * A[i]);
+            i++;
+        } else {
+            res.unshift(A[j] * A[j]);
+            j--;
+        }
+    }
+    return res;
 };
 sortedSquares();
