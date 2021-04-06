358fbb199bd9e7737112e7f973d1f1eb329f9288
Qcloud <qxwlxk@163.com>
2021-04-06 11:25:22 +0800

    [docs]Add question80

---
+++ question80.js
@@ -0,0 +1,21 @@
+/**
+ * @name Remove Duplicates from Sorted Array II
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function (nums) {
+    let len = nums.length;
+    if (len <= 2) {
+        return len;
+    }
+    let slow = 2,
+        fast = 2;
+    while (fast < len) {
+        if (nums[fast] !== nums[slow - 2]) {
+            nums[slow] = nums[fast];
+            slow++;
+        }
+        fast++;
+    }
+    return slow;
+};
